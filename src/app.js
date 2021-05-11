36998dbcec73c9a2967faec182ab224bc02e9d08
hermanjackson <hermanjackson561@gmail.com>
2021-05-11 17:29:59 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function digitalclock() {
+  var date = new Date();
+  var hours = date.getHours() + "";
+  var minutes = date.getMinutes() + "";
+  var seconds = date.getSeconds() + "";
+  var day = date.getDay();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  if (hours.length < 2) {
+    hours = "0" + hours;
+  }
+  if (minutes.length < 2) {
+    minutes = "0" + minutes;
+  }
+  if (seconds.length < 2) {
+    seconds = "0" + seconds;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  var weekdays = ["sun", "mon", "tue", "wed", "thur", "frid", "sat"];
+
+  var clock = weekdays[day] + ":" + hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("clock").innerHTML = clock;
+}
+digitalclock();
+setInterval(digitalclock, 1000);
